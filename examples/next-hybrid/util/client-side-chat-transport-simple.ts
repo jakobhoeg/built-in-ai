571159f170f2dde26d95c3368ae8d2029a786c16
Jakob Hoeg <jakobhoegm@gmail.com>
2025-07-20 13:34:55 +0200

    add old simple chat-transport

---
+++ examples/next-hybrid/util/client-side-chat-transport-simple.ts
@@ -0,0 +1,47 @@
+import {
+  ChatTransport,
+  UIMessage,
+  UIMessageChunk,
+  streamText,
+  convertToModelMessages,
+  ChatRequestOptions,
+
+} from "ai";
+import { builtInAI } from "@built-in-ai/core";
+
+// This class won't stream back data parts with the download progress if
+// the Prompt API model hasn't yet been downloaded
+export class ClientSideChatTransport implements ChatTransport<UIMessage> {
+  async sendMessages(
+    options: {
+      chatId: string;
+      messages: UIMessage[];
+      abortSignal: AbortSignal | undefined;
+    } & {
+      trigger:
+      | "submit-user-message"
+      | "submit-tool-result"
+      | "regenerate-assistant-message";
+      messageId: string | undefined;
+    } & ChatRequestOptions,
+  ): Promise<ReadableStream<UIMessageChunk>> {
+    const prompt = convertToModelMessages(options.messages);
+
+    const result = streamText({
+      model: builtInAI(),
+      messages: prompt,
+      abortSignal: options.abortSignal,
+    });
+
+    return result.toUIMessageStream();
+  }
+
+  async reconnectToStream(
+    options: {
+      chatId: string;
+    } & ChatRequestOptions,
+  ): Promise<ReadableStream<UIMessageChunk> | null> {
+    // AFAIK: Client-side AI doesn't support stream reconnection
+    return null;
+  }
+}
